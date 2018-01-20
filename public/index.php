a2157d3d5098fe66836aef08fe8aaf8c3fc1a473
Solber <solber@hotmail.fr>
2018-01-21 00:23:15 +0100

    some updates

--- public/index.php
+++ public/index.php
@@ -11,7 +11,17 @@ $app = new \Slim\App([
 require '../app/container.php';
 
 $app->get('/', \App\Controllers\PageController::class . ':home')->setName('home');
+
 $app->get('/login', \App\Controllers\PageController::class . ':login')->setName('login');
 $app->post('/login', \App\Controllers\PageController::class . ':postlogin');
+
+$app->get('/register', \App\Controllers\PageController::class . ':register')->setName('register');
+$app->post('/register', \App\Controllers\PageController::class . ':postregister');
+
+$app->get('/profile', \App\Controllers\PageController::class . ':profile')->setName('profile');
+$app->post('/profile', \App\Controllers\PageController::class . ':postprofile');
+
+$app->get('/suggestion', \App\Controllers\PageController::class . ':suggestion')->setName('suggestion');
+$app->post('/suggestion', \App\Controllers\PageController::class . ':suggestion');
 
 $app->run();
